38bf6622e9d237032506d04a6989e86435be3d23
vadav <149052355+vadav118@users.noreply.github.com>
2026-03-08 14:27:49 +0100

    final

--- HF4/NKiralyno.py
+++ HF4/NKiralyno.py
@@ -4,7 +4,7 @@ class Kiralynok:
     def __init__(self,ke,c):
         self.kezdo = ke
         self.cel = c
-        self.n = len(ke[0])
+        self.n = len(ke[0])-1
 
 
     def celteszt(self,a):
@@ -12,9 +12,9 @@ class Kiralynok:
 
     def rakovetkezo(self,t):
         gyerekek = []
-        s = t[1] - 1
-        a = t[0].copy()
-        for i in range(self.n):
+        s = t[1] - 1 #nullával való indexelés miatt
+        a = t[0].copy() #
+        for i in range(self.n+1):
             elofeltetel = True
             if 1 in a[:s,i]:
                 elofeltetel= False
@@ -28,7 +28,7 @@ class Kiralynok:
                 k -=1
 
             m, k = s - 1, i + 1
-            while m >= 0 and k < self.n:
+            while m >= 0 and k <= self.n:
                 if a[m,k] == 1:
                     elofeltetel = False
                     break
@@ -46,7 +46,7 @@ if __name__ == '__main__':
 
     tabla = np.zeros((8,8), dtype=int)
 
-    feladat = Kiralynok((tabla,1),8)
+    feladat = Kiralynok((tabla,1),9)
 
     allapotok = [feladat.kezdo]
 
