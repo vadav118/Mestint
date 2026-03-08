6d58f6339804e26554ff117337b1095d27eac59c
vadav <149052355+vadav118@users.noreply.github.com>
2026-03-08 15:16:54 +0100

    NQueens fixes

--- HF4/NKiralyno.py
+++ HF4/NKiralyno.py
@@ -12,14 +12,14 @@ class Kiralynok:
 
     def rakovetkezo(self,t):
         gyerekek = []
-        s = t[1] - 1 #nullával való indexelés miatt
+        s = t[1]
         a = t[0].copy() #
-        for i in range(self.n+1):
+        for i in range(1,self.n+2):
             elofeltetel = True
-            if 1 in a[:s,i]:
+            if 1 in a[:s-1,i-1]:
                 elofeltetel= False
 
-            m,k = s-1,i-1
+            m,k = s-2,i-2
             while m>=0 and k>=0:
                 if a[m,k] == 1:
                     elofeltetel = False
@@ -27,7 +27,7 @@ class Kiralynok:
                 m -=1
                 k -=1
 
-            m, k = s - 1, i + 1
+            m, k = s - 2, i
             while m >= 0 and k <= self.n:
                 if a[m,k] == 1:
                     elofeltetel = False
@@ -37,8 +37,8 @@ class Kiralynok:
 
             if elofeltetel:
                 uj_allpot = a.copy()
-                uj_allpot[s,i] = 1
-                gyerekek.append((uj_allpot,s+2))
+                uj_allpot[s-1,i-1] = 1
+                gyerekek.append((uj_allpot,s+1))
 
         return gyerekek
 
